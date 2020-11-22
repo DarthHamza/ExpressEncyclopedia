80e74525b4ef6b5c6e4c70c66f3087b1eaf8199b
DarthHamza <hamzamakia@gmail.com>
2020-11-22 23:06:08 +0300

    setup sequalize and created first model

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const cookieRoutes = require('./routers/cookieRoutes') 
+const cookieRoutes = require('./routers/cookieRoutes')
+const db = require('./db/models')
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/cookies', cookieRoutes);
 
-
 const PORT = 8000;
+
+const run = async () => {
+    try {
+        await db.sequelize.sync();
+        console.log("Connection to the database successful!");
+    } catch (error) {
+        console.error("Error connecting to the database: ", error);
+    }
+};
+
 app.listen(PORT, () => {
     console.log(`The application is running on localhost:${PORT}`);
 })
+  
+run();
---
+++ db/models/Cookie.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => sequelize.define("Cookie", {
+    name: {
+        type: DataTypes.STRING,
+    },
+    description: {
+        type: DataTypes.STRING,
+    },
+    price: {
+        type: DataTypes.INTEGER,
+    },
+    image: {
+        type: DataTypes.STRING,
+    },
+});
