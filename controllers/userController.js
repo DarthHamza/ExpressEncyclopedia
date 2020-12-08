2388903740bff1a688515cbf40cde696bd05365b
DarthHamza <hamzamakia@gmail.com>
2020-12-08 10:13:02 +0300

    generated token at signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,16 @@ exports.signup = async (req, res, next) => {
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         req.body.password = hashedPassword;
         const newUser = await User.create(req.body);
-        res.status(201).json({ message: "User created successfully"});
+        const payload = {
+            id: newUser.id,
+            username: newUser.username,
+            email: newUser.email,
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            expires: Date.now() + JWT_EXPIRATION_MS,
+        };
+        const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+        res.status(201).json({ token });
     } catch (error){
         next(error);
     }
